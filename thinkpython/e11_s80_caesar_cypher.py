5128f994057c29a99f26102228244c2e785fd45b
3ship <47889681+3ship@users.noreply.github.com>
2024-02-27 14:38:57 +0100

    docstring

--- thinkpython/e11_s80_caesar_cypher.py
+++ thinkpython/e11_s80_caesar_cypher.py
@@ -3,9 +3,12 @@ as parameters, and returns a new string that contains the letters from the
 original string rotated by the given amount."""
 
 from string import ascii_lowercase as lows
+# We could set all letters to .lower() and get rid off the elif statement
 from string import ascii_uppercase as caps
 
 def rotate_word(s, n):
+    """Takes the input string s and rotates every letter by n.
+    Using modulo 26 to 'wrap around' the alphabet string, if necessary."""
     new_s = ""
     for letter in s:
         if letter in lows:
