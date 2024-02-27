09951bca2122e99ef3ba2ee15c551ac371248071
3ship <47889681+3ship@users.noreply.github.com>
2024-02-27 14:26:56 +0100

    Added exercise 8.5

---
+++ thinkpython/e11_s80_caesar_cypher.py
@@ -0,0 +1,22 @@
+"""Write a function called rotate_word that takes a string and an integer
+as parameters, and returns a new string that contains the letters from the
+original string rotated by the given amount."""
+
+from string import ascii_lowercase as lows
+from string import ascii_uppercase as caps
+
+def rotate_word(s, n):
+    new_s = ""
+    for letter in s:
+        if letter in lows:
+            index = lows.find(letter)
+            new_s += lows[(index + n) % 26]
+        elif letter in caps:
+            index = caps.find(letter)
+            new_s += caps[(index + n) % 26]
+        else:
+            continue
+    return new_s
+
+if __name__=='__main__':
+    print(rotate_word('TestStringWithCaps', 3))
