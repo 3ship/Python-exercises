5a3c3523e2dc6c9323f072214328604473d9092f
3ship <47889681+3ship@users.noreply.github.com>
2024-04-05 13:38:04 +0200

    Add exercise 9.3 on page 84

---
+++ thinkpython/e14_s84_avoid_letters.py
@@ -0,0 +1,16 @@
+def avoids(word, verboten):
+    forbidden_letters = 0
+    for letter in verboten:
+        if letter in word:
+            forbidden_letters += 1
+        else:
+            continue
+    if forbidden_letters > 0:
+        return False
+    else:
+        return True
+
+if __name__=='__main__':
+    w = input('Enter a word: ')
+    v = input('Enter a string of forbidden letters: ')
+    print(avoids(w, v))
