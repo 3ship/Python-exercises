36e837e335dad4e5907c82283463d40fae8ad251
3ship <47889681+3ship@users.noreply.github.com>
2024-04-10 11:55:16 +0200

    Corrected exercise number

--- thinkpython/e17_s88_palindrome_numbers.py
+++ thinkpython/e17_s88_palindrome_numbers.py
@@ -1,5 +1,5 @@
 """Print all numbers in the six-digit range that form a palindrome.
-This is a simplified version of the exercise 9.9, to keep the output shorter
+This is a simplified version of the exercise 9.8, to keep the output shorter
 With additional string-slicing we could print all numbers where four or five
 of its digits form a palindrome. Example:
 str(number)[1:5] == str(number)[-1:-5:-1]
