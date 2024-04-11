b85f67c07e43424ac22252cbec41d12861078131
3ship <47889681+3ship@users.noreply.github.com>
2024-04-11 11:16:58 +0200

    Add explanation for 10.4

--- thinkpython/e18_s100_nested_sum.py
+++ thinkpython/e18_s100_nested_sum.py
@@ -1,12 +1,13 @@
 # 10.1: Add up all integers from nested lists
 # 10.3: Return a list without the first and last elements of the input list
-
-def middle(t):
-    return t[1:-1]
+# 10.4: Remove the first and last element of the list and return None
 
 def chop(t):
     t.pop(0)
     t.pop(-1)
+
+def middle(t):
+    return t[1:-1]
 
 def nested_sum(t):
     total_sum = 0
